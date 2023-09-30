1a25f9eb5ae72d2d3850f876b3b84e60d9ef51f5
williamfoschiera <william.foschiera@buser.com.br>
2023-09-30 15:54:57 -0400

    linter.

--- demo_app/basic_canvas.py
+++ demo_app/basic_canvas.py
@@ -1,18 +1,18 @@
 import logging
-from .tasks import add, mult
 
 from celery import signature
 
+from .tasks import add
+
 logger = logging.getLogger("playground")
 
+
 def signature_example():
-    print(signature("tasks.add", args=(2,2), countdown=10))
+    print(signature("tasks.add", args=(2, 2), countdown=10))
     # is the same as
-    print(add.signature((2,2), countdown=10))
+    print(add.signature((2, 2), countdown=10))
     # and using star arguments
-    print(add.s(2,2))
-
-
+    print(add.s(2, 2))
 
 
 if __name__ == "__main__":
--- django_celery_pg/celery.py
+++ django_celery_pg/celery.py
@@ -2,16 +2,8 @@ import os
 
 from celery import Celery
 
-from celery import shared_task
-
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_celery_pg.settings")
 
-celery_app = Celery("django_celery_pg.celery", include=[
-  "demo_app.tasks",
-  "demo_app.basic_canvas"
-  ])
+celery_app = Celery("django_celery_pg.celery", include=["demo_app.tasks", "demo_app.basic_canvas"])
 celery_app.config_from_object("django_celery_pg.settings_celery")
 celery_app.autodiscover_tasks()
-
-
