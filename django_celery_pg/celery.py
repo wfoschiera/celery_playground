31edd6218c3c230f845847a602418203beae8ac2
williamfoschiera <william.foschiera@buser.com.br>
2023-09-30 15:12:27 -0400

    remove unused code.

--- django_celery_pg/celery.py
+++ django_celery_pg/celery.py
@@ -1,10 +1,8 @@
 import os
 
-# import time
 from celery import Celery
 
 from celery import shared_task
-# from celery.signals import after_task_publish, before_task_publish
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_celery_pg.settings")
@@ -16,17 +14,4 @@ celery_app = Celery("django_celery_pg.celery", include=[
 celery_app.config_from_object("django_celery_pg.settings_celery")
 celery_app.autodiscover_tasks()
 
-
-# celery_app.conf.task_queues = (
-#     # Definindo uma fila com o nome 'default'
-#     celery_app.Queue('default', celery_app.Exchange('default'), routing_key='default'),
-
-#     # Definindo uma fila qualquer para testes
-#     celery_app.Queue('common', celery_app.Exchange('common'), routing_key='common'),
-
-#     # Definindo uma fila chamada 'priority' com alta prioridade
-#     celery_app.Queue('priority', celery_app.Exchange('priority'), routing_key='priority'),
-# )
-
-# celery_app.conf.task_default_queue = 'default'
 
